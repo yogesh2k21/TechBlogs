5cb8e57e4e59c50be576273c47b0de1aab1d8f2b
Yogesh <52989607+yogesh2k21@users.noreply.github.com>
2021-07-14 15:42:58 +0530

    Update settings.py

--- TechBlog/settings.py
+++ TechBlog/settings.py
@@ -135,10 +135,10 @@ STATIC_ROOT=BASE_DIR/"static"
 # STATICFILES_DIRS = (
 #     os.path.join(BASE_DIR, 'static'),
 # )
-# STATICFILES_DIRS = [
-#     BASE_DIR / "static",
-#     '/var/www/static/',
-# ]
+ STATICFILES_DIRS = [
+     BASE_DIR / "static",
+     '/var/www/static/',
+ ]
 
 #managing media
 MEDIA_URL="/media/"   #this add media in URL
